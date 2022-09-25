6b3779bb3086af3f4e089ae3cf98ba383d13d874
TheGrogy <113811799+TheGrogy@users.noreply.github.com>
2022-09-25 13:06:53 +1000

    Add files via upload

--- index.php
+++ index.php
@@ -1,42 +1,56 @@
 <?php
 
-$array = [
+$users = [
     [
         'firstName' => 'Den',
         'age' => 16,
         'SecondName' => 'funny type',
         
     ],
-
     [
         'firstName' => 'Dima',
         'age' => 14,
-        'SecondName' => 'genius of minecraft',
+        'second_Name' => 'genius of minecraft',
         
     ],
-
     [
         'firstName' => 'DIma',
         'age' => 16,
-        'SecondName' => 'general Dima',
+        'second_Name' => 'general Dima',
         
     ],
-
     [
         'firstName' => 'Sergey' ,
         'age' => 16,
-        'SecondName' => 'simple genius',
+        'second_Name' => 'simple genius',
         
     ],
 ];
 
 print('<pre>');
-print_r($array);
-print('<pre>');
+print_r($users);
 
-function show($array)
-{
-    echo "$array[0]";
+function show():array {
+    return [
+        'firstName' => 'Dima',
+        'age' => 14,
+        'second_Name' => 'genius of minecraft'
+    ];
 }
 
-?>
+print_r(show());
+print('</pre>');
+
+function get_users():array {
+    $userslist = [];
+
+    for ($i = 5; $i <= 10; $i++) {
+        $userslist[] = show();
+    }
+
+    return $userslist;
+}
+
+print('<pre>');
+print_r(get_users());
+print('</pre>');
